e0b87d777b2c9cb3482aafb28cbb98d1dae1b18a
Cidoni Brind <cidoni@gmail.com>
2018-12-19 20:52:52 -0600

    Changed to call the correct python program

--- newsticker.sh
+++ newsticker.sh
@@ -9,4 +9,4 @@ fi
 
 # run pub/sub sample app using certificates downloaded in package
 printf "\nRunning pub/sub sample application...\n"
-python3 aws-iot-device-sdk-python/samples/basicPubSub/basicPubSub.py -e a3q6yyaptqkmga-ats.iot.us-east-1.amazonaws.com -r root-CA.crt -c newsTicker.cert.pem -k newsTicker.private.key -t newstickerTopic
+python3 /home/pi/risrim/awsiot/newsticker_iot.py -e a3q6yyaptqkmga-ats.iot.us-east-1.amazonaws.com -r root-CA.crt -c newsTicker.cert.pem -k newsTicker.private.key -t newstickerTopic
