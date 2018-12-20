f124a28f8b6081ad707741f879e186c35cbd4f56
Cidoni Brind <cidoni@gmail.com>
2018-12-19 20:53:17 -0600

    Fixed the received message parsing issue

--- newsticker_iot.py
+++ newsticker_iot.py
@@ -25,11 +25,12 @@ AllowedActions = ['both', 'publish', 'subscribe']
 
 # Custom MQTT message callback
 def customCallback(client, userdata, message):
-    print("Received a new message: ")
-    print(message.payload)
-    print("from topic: ")
-    print(message.topic)
-    print("--------------\n\n")
+    #print("Received a new message: ")
+    msg=message.payload.decode('utf-8')
+    print (msg)
+    #print("from topic: ")
+    #print(message.topic)
+    #print("--------------\n\n")
 
 
 # Read in command-line parameters
@@ -79,7 +80,7 @@ if not args.useWebsocket and not args.port:  # When no port override for non-Web
 
 # Configure logging
 logger = logging.getLogger("AWSIoTPythonSDK.core")
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.ERROR)
 streamHandler = logging.StreamHandler()
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 streamHandler.setFormatter(formatter)
@@ -112,6 +113,7 @@ time.sleep(2)
 # Publish to the same topic in a loop forever
 loopCount = 0
 while True:
+    '''
     if args.mode == 'both' or args.mode == 'publish':
         message = {}
         message['message'] = args.message
@@ -121,4 +123,6 @@ while True:
         if args.mode == 'publish':
             print('Published topic %s: %s\n' % (topic, messageJson))
         loopCount += 1
+    '''
     time.sleep(1)
+
